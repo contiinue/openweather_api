df4dab5241bb483dede0c613b532fbfdecd856de
main <shapilov04@mail.ru>
2023-02-03 22:08:41 +0300

    build: add endpoint get_city_stats for get all weathers of city

--- src/weather/schemas.py
+++ src/weather/schemas.py
@@ -1,11 +1,1 @@
-from pydantic import BaseModel
-
-
-class CitySchemas(BaseModel):
-    id: int
-    name: str
-    city_id: int
-    weather_id: int
-
-    class Config:
-        orm_mode = True
+""" Here pydantic need models ;)) """
